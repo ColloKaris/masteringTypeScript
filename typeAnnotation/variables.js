3190945eea669a58ebedc06ef91619e59a5165e9
Collins Kariuki <collinsmunkary@gmail.com>
2023-02-11 05:09:42 +0300

    Type Inference

---
+++ typeAnnotation/variables.js
@@ -0,0 +1,6 @@
+var movieTitle = "Amadeus";
+movieTitle = "Arrival";
+var numCatLives = 9;
+numCatLives += 1;
+var gameOver = false;
+gameOver = true;
--- typeAnnotation/variables.ts
+++ typeAnnotation/variables.ts
@@ -6,3 +6,13 @@ numCatLives += 1;
 
 let gameOver: boolean = false;
 gameOver = true;
+
+
+//Type Inference
+let tvShow = "Olive Kitteridge";
+tvShow = "The other two";
+//tvShow = false;
+
+let isFunny = false;
+isFunny = true;
+//isFunny = "asd";
