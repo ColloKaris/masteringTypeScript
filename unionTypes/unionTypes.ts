b20a7a3080a09aad5aba3e91a3e771c33b4f14ca
Collins Kariuki <collinsmunkary@gmail.com>
2023-02-20 05:57:34 +0300

    Type Narrowing

--- unionTypes/unionTypes.ts
+++ unionTypes/unionTypes.ts
@@ -15,3 +15,19 @@ type Loc = {
 let coordinates: Point | Loc = {x: 1, y: 34};
 coordinates = {lat: 321, long: 23};
 
+//TYPE NARROWING WITH UNION TYPES
+function printAge(age: number | string): void {
+    console.log(`You are are ${age} years old`); 
+}
+
+printAge(23);
+printAge("87");
+
+function calculateTax(price: number | string, tax: number){
+    //return price * tax
+    //price.replace("$", "")
+    if(typeof price === "string"){
+        price = parseFloat(price.replace("$", ""));
+    }
+    return price * tax;
+}
