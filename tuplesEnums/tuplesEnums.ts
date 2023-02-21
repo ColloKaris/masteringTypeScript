61afc0bd5ece2122ee6d727b1b85e3120231b19f
Collins Kariuki <collinsmunkary@gmail.com>
2023-02-21 05:33:07 +0300

    Intro to tuples and enums

---
+++ tuplesEnums/tuplesEnums.ts
@@ -0,0 +1,22 @@
+//creating a tuple - tuples have a fixed length, type and the order
+//or elements matter
+const color: [number, number, number ] = [255, 0, 255];
+
+//we can have mixed type tuples and the order matters
+type HTTPResponse = [number, string];
+const goodRes: HTTPResponse = [200, "OK"];
+
+//Enums
+enum OrderStatus {
+    PENDING,
+    SHIPPED,
+    DELIVERED,
+    RETURNED
+}
+const myStatus = OrderStatus.DELIVERED;
+
+function isDelivered(status: OrderStatus){
+    return status === OrderStatus.DELIVERED
+}
+
+isDelivered(OrderStatus.DELIVERED)
