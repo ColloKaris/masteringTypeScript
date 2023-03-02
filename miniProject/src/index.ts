45c6b1bc4c84e0f6fcfddb64e5fcaf9b95cea377
Collins Kariuki <collinsmunkary@gmail.com>
2023-03-02 10:59:45 +0300

    Type Assertions

--- miniProject/src/index.ts
+++ miniProject/src/index.ts
@@ -4,3 +4,8 @@ btn.addEventListener("click", function() {
     alert("CLICKED!!");
 });
 
+//TYPE ASSERTIONS
+let mystery: unknown = "Hello World!!!";
+
+const numChars = (mystery as string).length
+
