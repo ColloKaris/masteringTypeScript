8c12352a7d65fdbbdb1daff068652c71efa2e51e
Collins Kariuki <collinsmunkary@gmail.com>
2023-03-04 09:20:19 +0300

    Working with events

--- miniProject/src/index.ts
+++ miniProject/src/index.ts
@@ -1,10 +1,9 @@
 const btn = document.getElementById("btn")! as HTMLButtonElement;
 const input = document.getElementById("todoinput")! as HTMLInputElement;
-input
-
-btn.addEventListener("click", function() {
-    alert(input.value);
-    input.value = "";
-});
+const form = document.querySelector("form")!;
 
 
+form.addEventListener("submit", (e) => {
+    e.preventDefault();
+    console.log("submitted")
+})
