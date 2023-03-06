294360d659d85988b8c9d39a57bcda9a811ff51a
Collins Kariuki <collinsmunkary@gmail.com>
2023-03-06 08:53:17 +0300

    Adding in an interface

--- miniProject/src/index.ts
+++ miniProject/src/index.ts
@@ -1,19 +1,35 @@
+interface Todo{
+    text: string;
+    completed: boolean;
+}
+
 const btn = document.getElementById("btn")! as HTMLButtonElement;
 const input = document.getElementById("todoinput")! as HTMLInputElement;
 const form = document.querySelector("form")!;
 const list = document.querySelector("ul")!;
 
 
+const todos: Todo[] = [];
+
+
 form.addEventListener("submit", (e) => {
     e.preventDefault();
-    const newTodoText = input.value;
+    const newTodo: Todo = {
+        text: input.value,
+        completed: false
+    }
+    createTodo(newTodo)
+    todos.push(newTodo);
+    input.value = '';
+})
+
+function createTodo(todo: Todo){
     const newLI = document.createElement("li");
     const checkbox = document.createElement("input");
     checkbox.type = "checkbox";
-    newLI.append(newTodoText);
+    newLI.append(todo.text);
     newLI.append(checkbox);
     list.append(newLI);
-    input.value = '';
-})
+}
 
 //Sunday did not code but tomorrow I am back at it
