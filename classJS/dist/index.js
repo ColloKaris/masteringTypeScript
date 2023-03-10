e6b58f1d4238a0606e9744400401f7151e3a2559
Collins Kariuki <collinsmunkary@gmail.com>
2023-03-10 15:46:16 +0300

    The class keyword

--- classJS/dist/index.js
+++ classJS/dist/index.js
@@ -1,2 +1,12 @@
-"use strict";
-console.log("It is working!!!!");
+class Player {
+    taunt(){
+        console.log("BOOYAH!")
+    }
+}
+
+const player1 = new Player();
+console.log(player1);
+player1.taunt();
+
+const player2 = new Player();
+player2.taunt();
