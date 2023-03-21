d91193054504d90ea3237a2b021556ac9261f8f8
Collins Kariuki <collinsmunkary@gmail.com>
2023-03-21 06:15:00 +0300

    Annotating classes in TypeScript

--- classJS/src/index.ts
+++ classJS/src/index.ts
@@ -1,1 +1,10 @@
-console.log("It is working!!!!");
+class Player {
+    first: string;
+    last: string;
+    constructor(first: string,last: string) {
+        this.first = first;
+        this.last = last;
+    }
+}
+
+const elton = new Player("Elton", "Steele");
