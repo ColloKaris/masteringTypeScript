0aaaa676ae2b853fc59ac6e5f31781fd6f072594
Collins Kariuki <collinsmunkary@gmail.com>
2023-03-21 06:48:47 +0300

    readonly, public and private properties

--- classJS/src/index.ts
+++ classJS/src/index.ts
@@ -1,9 +1,14 @@
 class Player {
-    first: string;
-    last: string;
+    public readonly first: string;
+    public readonly last: string;
+    private score: number = 0;
     constructor(first: string,last: string) {
         this.first = first;
         this.last = last;
+    }
+
+    private secretMethod(): void {
+        console.log("SECRET METHOD!")
     }
 }
 
