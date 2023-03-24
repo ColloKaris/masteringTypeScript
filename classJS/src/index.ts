d0dd0efd7c08ee9cdeb067528726792b4f5944af
Collins Kariuki <collinsmunkary@gmail.com>
2023-03-24 03:08:53 +0300

    Getters and setters

--- classJS/src/index.ts
+++ classJS/src/index.ts
@@ -20,12 +20,29 @@ class Player {
     constructor(
         public first: string,
         public last: string,
-        private score: number) {}
+        private _score: number) {}
 
     private secretMethod(): void {
         console.log("SECRET METHOD!")
     }
+
+    get fullName(): string {
+        return `${this.first} ${this.last}`
+    }
+
+    get score(): number {
+        return this._score;
+    }
+
+    set score(newScore: number) {
+        if(newScore < 0){
+            throw new Error ("Score cannot be negative!")
+        }
+        this._score = newScore;
+    }
+
 }
 
-const elton = new Player("Elton", "Steele");
-//hello
+const elton = new Player("Elton", "Steele", 100);
+elton.fullName;
+elton.score = 99;
