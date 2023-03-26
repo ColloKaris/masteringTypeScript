bf8c1fc2a2a8aa0a664cc374171fcceffac5647c
Collins Kariuki <collinsmunkary@gmail.com>
2023-03-26 18:54:21 +0300

    writing another generic function

--- Generics/index.ts
+++ Generics/index.ts
@@ -10,9 +10,22 @@ function booleanIdentity(item: boolean): boolean {
     return item;
 }
 
+interface Cat {
+    name: string;
+    breed: string;
+}
+
 //GENERIC FUNCTION
 function identity<Type>(item: Type): Type {
     return item;
 }
 
 identity<string>("7")
+
+//ANOTHER GENERIC ELEMENT
+function getRandomElement<T>(list: T[]): T{
+    const randIdx = Math.floor(Math.random() * list.length);
+    return list[randIdx];
+}
+
+getRandomElement<string>(["hello", "from", "the", "other"])
