78d181b5c7c4453eabde5a5445368153382014ca
Collins Kariuki <collinsmunkary@gmail.com>
2023-09-08 08:10:46 +0300

    Modules

---
+++ modules/dist/index.js
@@ -0,0 +1,3 @@
+import { add, sample } from "./utils.js";
+console.log(add(23, 4));
+console.log(sample([12, 3, 34]));
---
+++ modules/src/index.ts
@@ -0,0 +1,4 @@
+import {add, sample} from "./utils.js";
+import User, { userHelper } from "./User.js";
+console.log(add(23,4));
+console.log(sample([12,3,34]));
